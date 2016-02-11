aae0fb80c020919dfee9f64c18909b0a270b5599
mjbright <github.com@mjbright.net>
2016-02-11 11:24:05 +0100

    Added my own wrapper script - taking CV input from my planning.xlsx

---
+++ mycreate_cv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pdflatex=/c/Progs/texlive/2015/bin/win32/pdflatex.exe
+
+# By default: use CV.xlsx in local directory
+MYCV_XL=CV.xlsx
+
+MONTH=$(date +%04Y-%02m)
+PLANNING_DIR='d:/z/Dropbox/z/Planning/2016/'
+
+MYCV_XL="${PLANNING_DIR}/${MONTH}.xlsx"
+
+create_cv.sh -xl $MYCV_XL
+
