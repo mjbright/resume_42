43695c1107d84221fb427efdec0bda44cd46a904
Mike Bright <github@mjbright.net>
2016-05-09 11:41:40 +0000

    Parametrized docker build script for Dockerfile and image name

--- docker_build.sh
+++ docker_build.sh
@@ -5,19 +5,25 @@ die() {
     exit 1
 }
 
+#IMAGE_TAG=mjbright/alpine_cv_resume_42
+#DOCKERFILE=Dockerfile.alpine
+IMAGE_TAG=mjbright/cv_resume_42
+DOCKERFILE=Dockerfile
+DOCKERHUB_USER=mjbright
+
 echo; echo "---- build image: ----"
-CMD="docker build -t mjbright/cv_resume_42 ."
+#CMD="docker build -t $IMAGE_TAG ."
+CMD="docker build -f $DOCKERFILE -t $IMAGE_TAG ."
 echo $CMD
 $CMD || die "Build failed"
 
 echo; echo "---- login to DockerHub ----"
-CMD="docker login -u mjbright"
+CMD="docker login -u $DOCKERHUB_USER"
 echo $CMD
 $CMD || die "Login failed"
 
 echo; echo "---- push new image to DockerHub ----"
-CMD="docker push mjbright/cv_resume_42 "
+CMD="docker push $IMAGE_TAG"
 echo $CMD
 $CMD
-
 
