a9670a2cc86c3c46ffb11d3c189e50eade21df97
mjbright <github.com@mjbright.net>
2017-05-15 20:13:20 +0200

    Separated build from push funtions
    Moved code into functions

--- docker_build.sh
+++ docker_build.sh
@@ -12,13 +12,12 @@ DOCKERHUB_USER=mjbright
 #IMAGE_TAG=mjbright/alpine_cv_resume_42
 #DOCKERFILE=Dockerfile.alpine
 
-die() {
+function die {
     echo "$0: die - $*" >&2
     exit 1
 }
 
-press()
-{
+function press {
     [ ! -z "$1" ] && echo "$*"
     echo "Press <return> to continue"
     read _DUMMY
@@ -27,29 +26,38 @@ press()
 }
 
 
-echo; echo "---- build image: ----"
-#CMD="docker image build -t $IMAGE_TAG ."
-CMD="docker image build $* -f $DOCKERFILE -t $IMAGE_TAG_LATEST ."
-echo $CMD
-$CMD || die "Build failed"
+function build {
+    echo; echo "---- build image: ----"
+    #CMD="docker image build -t $IMAGE_TAG ."
+    CMD="docker image build $* -f $DOCKERFILE -t $IMAGE_TAG_LATEST ."
+    echo $CMD
+    $CMD || die "Build failed"
 
-echo; echo "---- date tag image: ----"
-CMD="docker image tag $IMAGE_TAG_LATEST $IMAGE_TAG_DATED"
-echo $CMD
-$CMD || die "Dated tag failed"
+    echo; echo "---- date tag image: ----"
+    CMD="docker image tag $IMAGE_TAG_LATEST $IMAGE_TAG_DATED"
+    echo $CMD
+    $CMD || die "Dated tag failed"
+}
 
-echo; press "About to login to Dockerhub"
-#echo; echo "---- login to DockerHub ----"
-CMD="docker login -u $DOCKERHUB_USER"
-echo $CMD
-$CMD || die "Login failed"
+function docker_push {
+    echo; press "About to login to Dockerhub"
+    #echo; echo "---- login to DockerHub ----"
+    CMD="docker login -u $DOCKERHUB_USER"
+    echo $CMD
+    $CMD || die "Login failed"
 
-echo; echo "---- push new image to DockerHub ----"
-CMD="docker image push $IMAGE_TAG_LATEST"
-echo $CMD
-$CMD
-CMD="docker image push $IMAGE_TAG_DATED"
-echo $CMD
-$CMD
+    echo; echo "---- push new image to DockerHub ----"
+    CMD="docker image push $IMAGE_TAG_LATEST"
+    echo $CMD
+    $CMD
+    CMD="docker image push $IMAGE_TAG_DATED"
+    echo $CMD
+    $CMD
+}
 
+if [ "$1" = "-push" ]; then
+    docker_push
+else
+    build
+fi
 
